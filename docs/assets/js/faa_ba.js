caeff019a2447e8f0b5486e633c486fac63b2537
Kai <kaisoapbox@gmail.com>
2024-01-30 11:19:39 -0800

    feat: added the rest of the faa ba questions

--- docs/assets/js/faa_ba.js
+++ docs/assets/js/faa_ba.js
@@ -2,6 +2,48 @@ const parent = document.getElementById("parent");
 
 window.onload = async function () {
   const questions = await (await fetch("/assets/data/faa_ba.json")).json();
+
+  function randomCompliment() {
+    const compliments = [
+      "Are you an NBCFAE member?",
+      "You're good at this.",
+      "Maybe you do have what it takes after all.",
+      "Are you cheating?",
+      "Lucky guess.",
+      "You're practically tailor-made for the FAA.",
+      "You're killing it.",
+      "You're doing great.",
+      "I guess you're ATC material then.",
+      "Maybe you were an ATC in a past life.",
+      "If you don't think about it too hard, this basically makes you Kennedy Steve.",
+    ];
+    const random = Math.floor(Math.random() * compliments.length);
+    return compliments[random];
+  }
+
+  function randomAdmonishment() {
+    const admonishments = [
+      "You should've known.",
+      "Are you just guessing?",
+      "You'll get it next time.",
+      "I'm not sure you're cut out for this.",
+      "That's got to sting.",
+      "Ouch.",
+      "Try thinking harder before answering the next one.",
+      "Are you sure you're trying?",
+      "I don't think you're ATC material.",
+      "Guess you're not what the FAA is looking for.",
+      "Have you considered lying?",
+      "Try lying next time.",
+      "Next time, try selecting the correct answer.",
+      "I recommend choosing the correct answer next time.",
+      "That one was a doozy!",
+      "Tricky one.",
+      "Hope you kept the day job.",
+    ];
+    const random = Math.floor(Math.random() * admonishments.length);
+    return admonishments[random];
+  }
   class Quiz extends React.Component {
     constructor(props) {
       super(props);
@@ -16,17 +58,19 @@ window.onload = async function () {
     handleClick(option, allOptions) {
       console.log(option);
       console.log(allOptions);
-      let nextStatus = "This question was worth no points, and didn't matter.";
+      let nextStatus = "That question was worth no points, and didn't matter.";
       let bestWeight = 0;
       allOptions.forEach((_option, _index) => {
         if (_option["weight"] > bestWeight) {
           bestWeight = _option["weight"];
           nextStatus =
             bestWeight === option["weight"]
-              ? `You picked the best option, worth ${_option["weight"]} points!`
+              ? `You picked the best option, worth ${
+                  _option["weight"]
+                } points! ${randomCompliment()}`
               : `The best option was '${String.fromCharCode(65 + _index)}. ${
                   _option["description"]
-                }', worth ${_option["weight"]} points.`;
+                }', worth ${_option["weight"]} points. ${randomAdmonishment()}`;
         }
       });
       this.setState((prevState) => ({
@@ -38,6 +82,13 @@ window.onload = async function () {
     }
 
     render() {
+      if (this.state.index === questions.length) {
+        return React.createElement(
+          "div",
+          null,
+          `Congratulations, you got ${this.state.score} out of a possible ${this.state.maxScore}. Remember, you need 114 to be able to become an ATC.`
+        );
+      }
       return React.createElement(
         "div",
         { id: "container" },
